33abb0774ff4fe71c14d9db50b0affa7d76b138d
tlvb25 <638@holbertonschool.com>
2019-02-07 23:22:16 +0000

    using if esle loops in c language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf(%d%n, "is positive\n");
+		printf("%d is positive\n",n);
 	}
 	else if (n == 0)
 	{
-		printf(%d%n, "is zero\n");
+		printf("%d is zero\n",n);
 	}
 	else
 	{
-		printf(%d%n, "is negative\n");
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
