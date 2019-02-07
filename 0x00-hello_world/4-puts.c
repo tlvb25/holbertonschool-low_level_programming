bc03e1be9eb3fc0425db2a1f7234985440383a22
tlvb25 <638@holbertonschool.com>
2019-02-07 18:49:08 +0000

    using function puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 
-/**                                                                             
- * main - Entry point                                                           
- *                                                                              
- * Return: Always 0 (Success)                                                   
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+	char str[50];
+
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+	puts(str);
+	return (0);
 }
