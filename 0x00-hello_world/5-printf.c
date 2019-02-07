7fed6b0ee7041877c5dacacb6fc66bc5514c67a3
tlvb25 <638@holbertonschool.com>
2019-02-07 18:41:11 +0000

    using just printf to print a string in c language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,10 @@
  *@void: void
  *
  *Description: main where all the code is called.
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
