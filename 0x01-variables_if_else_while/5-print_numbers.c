c96e5a875c752e823e49a2e1fff0800f3f0f0682
tlvb25 <638@holbertonschool.com>
2019-02-08 02:24:13 +0000

    printing number 1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *@void: void
+ *
+ *Description: main where all the code is called.
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+}
