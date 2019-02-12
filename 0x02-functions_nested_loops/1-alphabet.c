8b74fe39e829bcd05dc80d8075fae8e87783ab5e
tlvb25 <638@holbertonschool.com>
2019-02-12 02:22:04 +0000

    function that prints the alphabet in lowercase followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - function that prints the alphabet in lowercase
+ * @parameterx: Description of parameter x
+(* 
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+
+}
