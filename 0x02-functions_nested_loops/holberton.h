d057b97837c7c91471cbccb0f0bb4dccbf4fa4a7
tlvb25 <638@holbertonschool.com>
2019-02-13 23:22:51 +0000

    custom holberton header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#findef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +13,5 @@ void times_table(void);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
+
+#endif
