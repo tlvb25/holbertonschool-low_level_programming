6cfae3f82ce9304206374a42bc08fe6441659f96
tlvb25 <638@holbertonschool.com>
2019-02-13 23:26:24 +0000

    custom holberton header file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - provide every min of the day
+ * @void: parameter
+ *
+ *
+ * Description: Adds two integers
+ * Return: The sum of the 2 integers is returned
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+		}
