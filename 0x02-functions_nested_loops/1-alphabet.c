81e0405721154d050285a8455444de1e84345c32
tlvb25 <638@holbertonschool.com>
2019-02-13 23:34:29 +0000

    prints the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- * print_alphabet - function that prints the alphabet in lowercase
- * @parameterx: Description of parameter x
-(* 
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
+ * print_alphabet - provide every min of the day
+ * @void:
+ *
+ *
+ * Description: Adds two integers
+ * Return: The sum of the 2 integers is returned
+ */
 void print_alphabet(void)
 {
 	int n;
 
-	for(n = 'a'; n <= 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 		_putchar(n);
 	_putchar('\n');
 
