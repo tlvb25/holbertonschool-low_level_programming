102f4ac51d6102ca01544a8bde1dda89e46a2ba1
tlvb25 <638@holbertonschool.com>
2019-02-14 22:38:46 +0000

    adding holberton file

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADer
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+#endif
