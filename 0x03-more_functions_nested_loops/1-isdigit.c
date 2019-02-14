3d0d18ba83e97146addfd23792183164a0e05534
tlvb25 <638@holbertonschool.com>
2019-02-14 19:13:02 +0000

    check for digit

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isdigit - function that adds 2 integers
+ * @c: is one of the integers added together
+ *
+ * Description: finds the absolute value
+ * Return: The sum of the 2 integers is returned
+ */
+int _isdigit(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+}
