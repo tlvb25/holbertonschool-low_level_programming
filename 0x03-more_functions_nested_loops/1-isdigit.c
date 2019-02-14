35de0d68ba5c4fb0f905a1b65596437409dc4dca
tlvb25 <638@holbertonschool.com>
2019-02-14 22:48:58 +0000

    checks if n is a digit

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -8,12 +8,12 @@
  */
 int _isdigit(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
