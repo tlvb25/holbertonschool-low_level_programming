47ccd32b94ad57ae16fb14993b65182175ae6937
tlvb25 <638@holbertonschool.com>
2019-02-15 06:37:18 +0000

    customer header by Holberton

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_HEADER
 #define HOLBERTON_HEADer
+void print_square(int size);
+void print_diagonal(int n);
 void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
@@ -19,4 +21,5 @@ int add(int, int);
 void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int main(void);
 #endif
