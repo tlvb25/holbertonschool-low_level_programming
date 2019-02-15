5e5e65e78f0a02adb4efc7da987ffda0d981a97d
tlvb25 <638@holbertonschool.com>
2019-02-15 06:41:48 +0000

    fizz buzz test

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,28 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
+
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%i ", i);
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
-
 	printf("\n");
 	return (0);
 }
