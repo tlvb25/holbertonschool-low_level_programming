4b47f0f4a8b92d080878921b04a361654adf731d
tlvb25 <638@holbertonschool.com>
2019-02-16 07:50:52 +0000

    draws triangle according to number input

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,14 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
-	for (i  = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = i; j < (size - 1); j++)
-		{
 			_putchar(' ');
-		}
 		for (j = 0; j <= i; j++)
-		{
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
+
 }
