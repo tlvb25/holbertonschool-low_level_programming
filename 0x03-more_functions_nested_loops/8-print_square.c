41093f68bbfe463dc2081693d059249bdfba2a60
tlvb25 <638@holbertonschool.com>
2019-02-16 06:47:12 +0000

    prints the square root

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_square - function that adds 2 integers
+ * @size: the size to be squared
+ *
+ * Description: draws squares for input
+ * Return: no return value
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
