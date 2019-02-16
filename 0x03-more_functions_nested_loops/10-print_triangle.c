65c5e66561571e9a0ef0a56e7fbf0fd978908c48
tlvb25 <638@holbertonschool.com>
2019-02-16 07:34:48 +0000

    print triangle sizes according number input

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that adds 2 integers
+ * @size: is one of the integers added together
+ *
+ * Description: finds the absolute value
+ * Return: The sum of the 2 integers is returned
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i  = 0; i < size; i++)
+	{
+		for (j = i; j < (size - 1); j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
