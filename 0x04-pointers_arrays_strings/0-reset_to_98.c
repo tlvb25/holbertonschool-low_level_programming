1f5d65f00fd7958b7ea5802929ab8e60fbafa286
tlvb25 <638@holbertonschool.com>
2019-02-20 03:10:59 +0000

    pointer is set to the value 98

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that adds 2 integers
+ * @size: input variable
+ *
+ * Description: prints triangles
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
