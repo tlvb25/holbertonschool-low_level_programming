152a058d6bd2d683ee855bbb6d3d242e34606f2d
tlvb25 <638@holbertonschool.com>
2019-02-20 03:11:46 +0000

    adding readme and custom header file

---
+++ 0x04-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This projects contains exercises demonstrating arrays in functions
---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,27 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADer
+void reset_to_98(int *n);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int main(void);
+#endif
