e871c2573c4aca5cdbfd586dfacf50bc5c1b8aa0
tlvb25 <638@holbertonschool.com>
2019-02-20 07:16:32 +0000

    prints string reversed

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strlen - function that adds 2 integers
+ * @s: string pointer
+ *
+ * Description: prints triangles
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] > 0)
+	{
+		a++;
+	}
+	for (; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
