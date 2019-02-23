e1427f76ce6f5769cf805cc81a2c413c9380bbd0
tlvb25 <638@holbertonschool.com>
2019-02-23 08:00:29 +0000

    add file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,11 @@
 #ifndef HOLBERTON_HEADER
 #define HOLBERTON_HEADer
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
