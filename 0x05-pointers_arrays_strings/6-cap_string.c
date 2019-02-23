be159401f84b424f8e96dcb027981dde708ca028
tlvb25 <638@holbertonschool.com>
2019-02-23 07:59:16 +0000

    add file

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,29 @@
 #include "holberton.h"
-/**
- * *cap_string - function that adds 2 integers
- * @s: string pointer
- *
- * Description: prints triangles
- * Return: no return
- */
 
+/**
+ * cap_string - function capitalizes all words
+ * of a string
+ * @s: string to change
+ *
+ * Return: new string
+ */
 char *cap_string(char *s)
 {
-	int i = 0;
-	char pun[14] = {' ', '.', '\n', '\t', ',', ';', '!'
+	int i = 0, check = 0, j;
+	char ch[14] = {' ', '.', '\n', '\t', ',', ';', '!'
 		       , '?', '"', '(', ')', '{', '}'};
 
-	if (s[i] == pun[i])
-		s[i + 1] -= 32;
-	s[i + 1] = s[i + 1] - 32;
+	if (s[i] <= 122 && s[i] >= 97)
+		s[i] -= 32;
+	while (s[i] != '\0')
+	{
+		check = 0;
+		for (j = 0; j < 13; j++)
+			if (s[i] == ch[j])
+				check = 1;
+		if (check)
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+				s[i + 1] -= 32;
+		i++;
+	}
 	return (s);
-}
