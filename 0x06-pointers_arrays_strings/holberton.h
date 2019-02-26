25271e16b9d07ee7e46b4e7e32ce904c29cdf820
tlvb25 <638@holbertonschool.com>
2019-02-26 06:01:10 +0000

    updated holberon header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,9 @@
 #ifndef HOLBERTON_HEADER
 #define HOLBERTON_HEADer
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *cap_string(char *);
 char *string_toupper(char *);
