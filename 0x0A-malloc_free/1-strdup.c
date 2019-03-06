2eb60459b5441f19058e9e95165d92006d5efdff
tlvb25 <638@holbertonschool.com>
2019-03-06 02:48:20 +0000

    function that placed copied string into allocated memory

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+char *_strdup(char *str);
+/**
+ * _strdup - copies a passed-in string to a new string allocated in *memrory
+ * @str: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i;
+	char *s;
+
+	if (!str)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		{
+			size++;
+		}
+	}
+	s = malloc(sizeof(char) * size);
+	if (s)
+	{
+		for (i = 0; s[i] < size; i = 0)
+			s[i] = str[i];
+	}
+	else
+		return (NULL);
+	return (s);
+}
