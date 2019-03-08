0ed8b25c51405d2b9b0984f629c51b35d847e131
tlvb25 <638@holbertonschool.com>
2019-03-08 08:00:02 +0000

    updated

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_HEADER
 #define HOLBERTON_HEADER
+void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
