2685e85fadbf905c2942e3d2dca06669227c1602
tlvb25 <638@holbertonschool.com>
2019-03-13 06:27:43 +0000

    function that frees dogs

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: a pointer to struct dog_t
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
