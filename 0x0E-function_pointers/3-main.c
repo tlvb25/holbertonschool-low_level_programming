9f0557b49a27ff9c1473f17c47e5633d9664cc47
tlvb25 <638@holbertonschool.com>
2019-03-14 06:55:46 +0000

    updated

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -4,13 +4,52 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Pointer to an array containing the parameters
- * Return: Always zero
+ * main - a functioon that performs addition, subtraction,
+ * multiplication and division
+ * @argc: the number of arguments
+ * @argv: the arguments given
+ *
+ * Return: 0 Always (Success)
  */
+
 int main(int argc, char **argv)
 {
+	int num1, num2, result;
+	int (*pop)(int, int);
+	char optr;
 
+	/* Had Tu help me with strcmp and usage of function pointers */
+	/* Checks if the number of arguments is wrong */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	/* see's if the  operator in agrv[2] is correct */
+	pop = get_op_func(argv[2]);
+	if (!pop)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* handles if the user tries to divide (/ or %) by 0 */
+	optr = *argv[2];
+	if ((optr == '/' || optr == '%') && !(strcmp(argv[3], "0")))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	/* set the numbers to integer values*/
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	/* get the correct operator and calculate numbers as a integer */
+	result = get_op_func(argv[2])(num1, num2);
+
+	/* Print out the calculated number */
+	printf("%d\n", result);
+	return (0);
 }
