213e2aefafbdd7079b625dc79c450224ede8aad7
tlvb25 <638@holbertonschool.com>
2019-03-14 06:52:55 +0000

    main file for the 4 file task 3

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Pointer to an array containing the parameters
+ * Return: Always zero
+ */
+int main(int argc, char **argv)
+{
+
+
+}
