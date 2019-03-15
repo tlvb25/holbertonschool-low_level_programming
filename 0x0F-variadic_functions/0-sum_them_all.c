6069dc64d1f3e45d7d1b768c9ccfd9c345a04e2a
tlvb25 <638@holbertonschool.com>
2019-03-15 23:06:27 +0000

    updated

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list add;
+	va_list ap;
 	int unsigned i, sum;
 
-	va_start(add, n);
+	va_start(ap, n);
 
 	sum = 0;
 	if (n == 0)
 		return(0);
 	for (i = 0; i < n; i++)
-		    sum += va_arg(add, int);
-	va_end(add);
+		sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 }
---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints an integer
+ * @separator: the string printed between numbers
+ * @n: the number of integers passed
+ *
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int unsigned i, v;
+
+        va_start(ap, n);
+
+	for(i = 0; i < n; i++)
+	{
+		v = va_arg(ap, int);
+		if (separator)
+		{
+			printf("%d", v);
+			if (i < n -1)
+				printf("%s", separator);
+		}
+		else
+		{
+			return;
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
