c9c7727ea691e9c955bf1f8d63b434bc15033d7c
tlvb25 <638@holbertonschool.com>
2019-03-16 21:51:13 +0000

    updated

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,6 +2,20 @@
 #define VARIADIC_FUNC
 #include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * struct var - struct for function parameters to print anything
+ *
+ * @f: the function member
+ * @s: the string member
+ */
+
+typedef struct var
+{
+	char *s;
+	void (*f)(va_list ap);
+} var_struct;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
