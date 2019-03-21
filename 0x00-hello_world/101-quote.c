dd099044809655a5bc30b29a5a0e6232e3ccd42c
tlvb25 <638@holbertonschool.com>
2019-03-21 00:05:17 +0000

    changed the spacing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,20 @@
 #include <unistd.h>
+
 /**
  * main - Entry point to the program
- *@void: void
+ *@void:
  *
  *Description: prints to standard error
  *Return: 1
  */
+
 void main(void)
 {
-    char *i;
+  char *i;
 
-    i = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  i = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(2, i, 58);
+  write(2, i, 58);
 
-    return (1);
+  return (1);
 }
