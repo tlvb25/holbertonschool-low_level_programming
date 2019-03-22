2fa44d599fe0d051e569a8989572393e816ed598
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-21 23:25:22 -0700

    completed task 0 and 1

--- 0x11-singly_linked_lists/ 0-print_list.c
+++ 0x11-singly_linked_lists/ 0-print_list.c
@@ -7,5 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *temp = h;
+	int elements = 0;
 
+	if (temp == NULL)
+		return (0);
+	while (temp)
+	{
+		printf("[%i] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		elements++;
+	}
+	return (elements);
 }
--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -7,5 +7,13 @@
  */
 size_t list_len(const list_t *h)
 {
+    int elements = 0;
 
+    if (h == NULL)
+		return (0);
+	while (h)
+	{
+		elements++;
+	}
+	return (elements);
 }
