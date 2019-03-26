64cb0b2252c3611b40526c269b5a5a7e6ad0eb35
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 01:17:11 -0700

    corrections to make code work

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint -
+ * free_listint - frees memory allocated for listint_t
+ * @head: pointer to the list passed into function
  *
- * Return: 
+ * Return:
  */
 void free_listint(listint_t *head)
 {
+	listint_t *current;
+	listint_t *next;
 
+	current = head;
+
+	while (current)
+	{
+		next = current->next;
+		free(current->n);
+		free(current);
+		current = next;
+	}
+	head = NULL;
 }
