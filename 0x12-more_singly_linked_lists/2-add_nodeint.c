d63ca893fb3bb996c5d387512a0d46558475d635
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 00:18:30 -0700

    coded according requirements

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -1,11 +1,29 @@
 #include "lists.h"
 
 /**
- * add_nodeint -
+ * add_nodeint - function that adds a new node to the begining of list
+ * @head: the double pointer to the head node of the list
  *
- * Return: 
+ * Return: new_node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node;
 
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	if (!head)
+		return (NULL);
+	/* Put in the data/integer */
+	new_node->n = n;
+
+	/* makes new_node point to the node head pointed too */
+	new_node->next = *head;
+
+	/*Move head upward to now point to the new_node */
+	*head = new_node;
+
+	return(new_node);
 }
