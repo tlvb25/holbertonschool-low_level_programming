395496a67a7450a043287788032b12aa510bc10d
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-25 22:07:28 -0700

    updated header file

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
