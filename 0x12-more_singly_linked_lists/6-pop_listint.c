d5e1e6590f3c23c2359f4a353b9edc1d9b046b08
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 07:24:22 -0700

    code for task 6

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
-
+void
 /**
- * pop_listint -
+ * pop_listint - deletes head node and return data of head node
+ * @head: double pointer of the linked list passed to function
  *
- * Return: 
+ * Return: head node's data(n)
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *next_node;
+
+	if (!head)
+		return (0);
+	next_node->n = (*head)->n;
+	next_node->next = (*head)->next;
+	free(*head);
+	*head = next_node;
+
+	return ((*head)->n);
 
 }
