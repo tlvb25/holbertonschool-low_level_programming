b11ef2872aa167c62c2c571086b46b512d75526d
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 15:32:35 -0700

    code changes

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * listint_len - returns the number of elements in a linked listint_t list.
@@ -9,7 +8,7 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t i;
+	unsigned int i;
 
 	while (h)
 	{
