f822d18800c763eeffa9d9479ec9c9ef50387331
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 01:28:48 -0700

    coded according requirements

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - function that frees a listint_t list
+ * @head: the pointer to list passed to function
+ * 
+ * Return: nothing
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current = *head;
+	listint_t *temp;
 
+	while (current)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+	*head = NULL;
 }
