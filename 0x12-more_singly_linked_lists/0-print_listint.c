28ef4ac3fba3d3a9239237aff8b840dc11e28a54
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-25 20:53:44 -0700

    updated code

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -7,5 +7,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
+        unsigned int elements = 0;
 
+        if (h == NULL)
+        {
+                printf("Error\n");
+                return (1);
+        }
+        while (h)
+        {
+                elements++
+                h = h->next;
+        }
+return (i);
 }
