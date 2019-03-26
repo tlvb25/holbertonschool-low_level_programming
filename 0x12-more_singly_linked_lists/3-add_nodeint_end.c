8de65e0e119b15d6028a0a46e7e4c4338013b988
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 01:05:22 -0700

    coded task according requirements

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,33 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end -
+ * add_nodeint_end - adds a new node to end of list
+ * @head: the pointer to the head of the list passed to function
+ * @n: the integer value passed into the function
  *
- * Return: 
+ * Return: the new_node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new_node;
+	listint_t *last_node;
+	unsigned int i;
 
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		last_node = *head;
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
+	return (new_node);
 }
