40e5bda5eb0e152141ab17ca958b2ab89d56756c
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 18:27:01 -0700

    coded for task

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
+ * get_nodeint_at_index - function that returns the nth node of a linked list
+ * @head: linked list passed to function
+ * @index: the desired position of node in linked list
  *
- * Return: 
+ * Return: the data within the indicated-positioned node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+        /* This pointer is declare to keep from altering the *header pointer passed to function */
+        listint_t *temp;
+        /* This integer variable is used to count through to the desired index entered as parameter */
+        unsigned int count = 0;
 
+        temp = head;
+
+        while (temp)
+        {
+                if ( count == index)
+                        return (temp->n);
+                i++;
+                temp = temp->next;
+        }
+        return (0);
 }
