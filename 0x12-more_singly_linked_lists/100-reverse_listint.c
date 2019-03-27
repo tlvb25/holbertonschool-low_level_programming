b09e3fab6f1904c9d60769be3e029b62edab4b99
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-27 00:01:42 -0700

    code correction

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * reverse_listint - function that reverses a listint_t linked list.
- * @head: linked list passed into the function
+ * @head: a pointer to the beginning of the linked list
  *
  * Return: a pointer to the first node of the reversed list
  */
@@ -10,15 +10,15 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *next;
-	listint_t *prev = NULL;
+	listint_t *previous = NULL;
 
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
+		(*head)->next = previous;
+		previous = *head;
 		*head = next;
 	}
-	*head = prev;
+	*head = previous;
 	return (*head);
 }
