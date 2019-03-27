25c4e25269b1ccd06b00188fdc6189d033589421
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 23:51:49 -0700

    code correction

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,35 +9,37 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
+	listint_t *tmp;
 	listint_t *next_node;
 	unsigned int i;
+	
+	tmp = *head;
 
 	if (*head == NULL)
 		return (-1);
 	/* If head needs to be removed */
 	if (index == 0)
 	{
-		*head = temp->next; /* Change head */
-		free(temp); /* free old head  */
+		*head = tmp->next; /* Change head */
+		free(tmp); /* free old head  */
 		return (1);
 	}
 	/* Find previous node of the node to be deleted */
-	for (i = 0; temp != NULL && i < index - 1; i++)
+	for (i = 0; tmp != NULL && i < index - 1; i++)
 	{
-		temp = temp->next;
+		tmp = tmp->next;
 	}
 	/* If position is more than number of nodes */
-	if (temp == NULL || temp->next == NULL)
+	if (tmp == NULL || tmp->next == NULL)
 	{
 		return (-1);
 	}
-	/* Node temp->next is the node to be deleted */
+	/* Node tmp->next is the node to be deleted */
 	/* Store pointer to the next of node to be deleted */
-	next_node = temp->next->next;
+	next_node = tmp->next->next;
 	/* Unlink the node from linked list */
-	free(temp->next);
+	free(tmp->next);
 	/*Unlink the deleted node from list*/
-	temp->next = next_node;
+	tmp->next = next_node;
 	return (1);
 }
