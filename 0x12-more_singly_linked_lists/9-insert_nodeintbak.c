655d3bf5b24e63910e5b17f2f1dee62ce73056f8
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 23:43:53 -0700

    code correction

--- 0x12-more_singly_linked_lists/9-insert_nodeintbak.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeintbak.c
@@ -11,30 +11,31 @@ listint_t *getNode(int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp;
-	listint_t *newNode;
-	uint i;
+	listint_t *new_node;
+	unsigned int i;
 
-	temp = *head;
 
-	for (i = 0; temp && i < idx - 1; i++)
-		temp = temp->next;
-	if (temp || idx == 0 || i == idx - 1)
+	if (head)
 	{
-		newNode = malloc(sizeof(listint_t));
-		if (!newNode)
+		new_node = malloc(sizeof(listint_t));
+		if (!new_node)
 			return (NULL);
-		newNode->n = n;
-		if (idx == 0)
+		new_node->n = n;
+		new_node->next = NULL;
+
+		if (!idx)
+			return (NULL);
+		if (idx == 1)
 		{
-			newNode->next = *head;
-			*head = newNode;
+			new_node->next = *head;
+			*head = new_node;
 		}
-		else
-		{
-			newNode->next = temp->next;
-			temp->next = newNode;
-		}
-		return (newNode);
+		temp = *head;
+		for (i = 0; i < idx - 2; i++)
+			temp = temp->next;
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
