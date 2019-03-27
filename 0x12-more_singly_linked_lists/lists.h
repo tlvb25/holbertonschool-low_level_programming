84453e91bd9a0f0bcf09d03bdcc85add9f6f2b40
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 23:28:55 -0700

    correct header file

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -10,7 +10,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-listint_t *getNode(int n)
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
