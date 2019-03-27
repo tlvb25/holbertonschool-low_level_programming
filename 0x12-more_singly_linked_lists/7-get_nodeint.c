958327eebe93d7deb8b4888307e252fff830482f
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 18:51:38 -0700

    improved code styling

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	/* This pointer is declare to keep from altering the *header pointer passed to function */
+	/* This pointer keeps from altering original *header pointer */
 	listint_t *temp;
 	/* int variable used to cycle through to desired nth node in list */
 	unsigned int count = 0;
