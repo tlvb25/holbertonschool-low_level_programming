182354b3fad7b60b729ada78dba89c4c0c221511
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 23:23:24 -0700

    removed whitespaces

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,37 +4,37 @@ listint_t *getNode(int n);
  * insert_nodeint_at_index - function that inserts a new node at a given position.
  * @head: the linked list passed into function
  * @n: data passed into the function
- * @idx: desired node position 
+ * @idx: desired node position
  *
  * Return: addresss of new_node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *temp;
-        listint_t *new_node;
-        
+	listint_t *temp;
+	listint_t *new_node;
 
-        if (head)
-        {
-                new_node = malloc(sizeof(listint_t));
-                if (!new_mode)
-                        return (NULL);
-                new_node->n = n;
-                new_node->next = NULL;
 
-                if (!idx)
-                        return (NULL);
-                if (idx == 1)
-                {
-                        new_node->next = *head;
-                        head = new_node;
-                }
-                temp = *head;
-                for (int i = 0; i < idx - 2; i++)
-                        temp = temp->next;
-                new_node->next = temp->next;
-                temp->next = new_node;
-                return (newNode);
-        }
-        return (NULL);
-} 
+	if (head)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (!new_mode)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = NULL;
+
+		if (!idx)
+			return (NULL);
+		if (idx == 1)
+		{
+			new_node->next = *head;
+			head = new_node;
+		}
+		temp = *head;
+		for (int i = 0; i < idx - 2; i++)
+			temp = temp->next;
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (newNode);
+	}
+	return (NULL);
+}
