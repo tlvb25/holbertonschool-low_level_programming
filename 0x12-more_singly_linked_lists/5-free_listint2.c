c1554ab1a90bf57a31458588d7cb902a8380a165
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 17:27:49 -0700

    removing whitespaces

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -3,14 +3,14 @@
 /**
  * free_listint2 - function that frees a listint_t list
  * @head: the pointer to list passed to function
- * 
+ *
  * Return: nothing
  */
 void free_listint2(listint_t **head)
 {
 	listint_t **current = *head;
 
-	while ((*current = *head) != NULL);
+	while ((*current = *head) != NULL)
 	{
 		*head = head->next;
 		free(*current);
