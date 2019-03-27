fd05d3c3bbac2de6471ea91573fe3ca8a6df876b
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 17:43:12 -0700

    remove white spaces

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 /**
  * free_listint2 - function that frees a listint_t list
  * @head: the pointer to list passed to function
- * 
+ *
  * Return: nothing
  */
 void free_listint2(listint_t **head)
