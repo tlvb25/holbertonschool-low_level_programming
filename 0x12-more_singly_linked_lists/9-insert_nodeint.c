a424878f2fef0c0cc090dcc61d631feee5279e8b
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 23:33:20 -0700

    code correction

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 		new_node->next = temp->next;
 		temp->next = new_node;
-		return (newNode);
+		return (new_node);
 	}
 	return (NULL);
 }
