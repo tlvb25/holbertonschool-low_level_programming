981edca563ec5140c26849546fdf3e72c73812ce
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-26 20:47:36 -0700

    coded task 9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+listint_t *getNode(int n);
 /**
  * insert_nodeint_at_index -
  *
@@ -7,5 +7,46 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+        listint_t *temp;
+        listint_t *current = *head;
 
+        if (idx < 1 || idx > n + 1)
+                return (NULL);
+        else
+                /* Keep looping until the pos is zero */
+                while (idx--)
+                {
+                        if (idx == 0)
+                        {
+                                /* adding Node at required postion */
+                                temp = getNode(n);
+
+                                /* Making the new Node to point to */
+                                /* the old Node at the same position */
+                                temp->next = current;
+
+                                /* Changing the pointer of the Node previous */
+                                /* to the old Node to point to the new Node */
+                                current = temp;
+                        }
+                        else
+                                /* Assign double pointer variable to point to the */
+                                /* pointer pointing to the address of next Node */
+                                current = &(current->next);
+                        }
+                        n++;
+                }
+                return (current);
 }
+
+// function to create and return a Node 
+listint_t *getNode(int n) 
+{ 
+    // allocating space 
+    listint_t *newNode = newNode = malloc(sizeof(listint_t)); 
+  
+    // inserting the required data 
+    newNode->n = n; 
+    newNode->next = NULL; 
+    return newNode; 
+} 
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -10,6 +10,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+listint_t *getNode(int n)
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
