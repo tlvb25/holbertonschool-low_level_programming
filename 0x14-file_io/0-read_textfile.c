e8f0822a27309d100cbe0dce811989ecee7027eb
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 16:03:53 -0700

    wrote the code for task 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,11 +1,51 @@
 #include "holberton.h"
 
 /**
- * read_textfile -
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * @filename: the string passed into the function
+ * @letters: the number of letters
  *
- * Return: 
+ * Return: the actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+        ssize_t file2_read;
 
+        if (!filename || !letters)
+                return (0);
+
+/* since size_t letters is unknown I dynamically create space */
+
+        char *unknown_size = malloc(sizeof(char) * letters);
+        if (!unknown_size)
+                return (0);
+
+/* Opening the file passed into this fucntion using variable file2_read */
+        file2_read = open(filename, O_RDONLY, 0400);
+        if (file2_read == -1)
+        {
+                free(unknown_size);
+                return (0);
+        }
+
+/* reading the file passed into this function and stored in integer variable file2_read */
+        ssize_t read_file = read(file2_read, unknown_size, letters);
+        if (read_file == -1)
+        {
+                free(unknown_size);
+                return (0);
+        }
+        close (file2_read);
+
+/* writing the file passed into this function and stored in integer variable file2_read */
+        ssize_t written_file = write(STDOUT_FILENO, unknown_size, read_file);
+        if (written_file == -1)
+        {
+                free(unknown_size);
+                return (0);
+        }
+        
+        free(unknown_size);
+        return (written_file);
 }
