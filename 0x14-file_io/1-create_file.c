de9aceeb0b8700cb02acdc51a1d35febad9dbde7
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 18:04:05 -0700

    code corrections

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,10 +10,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t copied_file, created_file;
-	int i =0;
+	int i;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 
 /* creating & writing *filename into file descriptor */
 	created_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
