e35e84b79cc7d0f124cbf5a08af08cd6dcafb080
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 17:52:09 -0700

    code for task 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -7,5 +7,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	ssize_t copied_file, created_file;
+	int i;
 
+	if (filename == NULL)
+		return (0);
+
+/* creating & writing *filename into file descriptor */
+	created_file = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (created_file == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		copied_file = write(created_file, text_content, i);
+	}
+	close(created_file);
+	if (copied_file == -1)
+		return (-1);
+	return (1);
 }
