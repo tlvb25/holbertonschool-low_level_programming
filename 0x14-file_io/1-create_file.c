738c8351295f99cb6a7c38df73a33ec261433d59
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 17:39:24 -0700

    code for task 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,11 +1,32 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ * create_file - function that creates a file.
+ * @filename: the file to be copied to
+ * @text_content: the text file to copied from
  *
- * Return: 
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {
+	ssize_t copied_file, created_file;
+	int i;
 
+	if (filename == NULL)
+		return (0);
+
+/* creating & writing *filename into file descriptor */
+	created_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (created_file == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0;; i++)
+			copied_file = write(created_file, text_content, i);
+	}
+	close(created_file);
+	if (copied_file == -1)
+		return (-1);
+	return (1);
 }
