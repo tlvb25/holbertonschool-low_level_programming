77fbc957e97768ea342f20532740cf0e0b8f78b4
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 17:47:47 -0700

    code correction

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,8 +22,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (i = 0; text_content[i]; i++)
-			copied_file = write(created_file, text_content, i);
+		while (text_content[i])
+			i++;
+		copied_file = write(created_file, text_content, i);
 	}
 	close(created_file);
 	if (copied_file == -1)
