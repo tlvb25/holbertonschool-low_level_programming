f505351cc390b2e0eeebbf6850feed8a9a4ca4b9
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-02 18:12:52 -0700

    code corrections

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int i;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 /* creating & writing *filename into file descriptor */
 	created_file = open(filename, O_WRONLY | O_APPEND);
