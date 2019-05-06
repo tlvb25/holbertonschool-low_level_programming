20629fe6818a5a08b9805302c40ceed1a4829c83
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-05 23:43:05 -0700

    removed additional line

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -3,9 +3,26 @@
 /**
  * get_dnodeint_at_index -
  *
- * Return: 
+ * Return:
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *ptr = head;
+	size_t count = 1;
 
+	if (!head || !index)
+	{
+		return (head);
+	}
+
+	while (ptr)
+	{
+		if (count == index)
+		{
+			return (ptr->next);
+		}
+		ptr = ptr->next;
+		count++;
+	}
+	return (NULL);
 }
