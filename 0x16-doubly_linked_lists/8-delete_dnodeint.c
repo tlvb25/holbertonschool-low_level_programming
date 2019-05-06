586a496bf0b23171e5a4af2a125830d598c01d8b
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-06 11:28:10 -0700

    code for task 8

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,32 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
+ * delete_dnodeint_at_index - function that deletes node at index of a dlistint_t linked list
+ * @head: the linked list passed into function
+ * @index: the index of node to be deleted
  *
- * Return: 
+ * Return: 1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	int i;
 
+	if (!*head || index <= 0)
+		return (-1);
+
+	dlistint_t *current = *head;
+
+	for (i = 1; current && i < index; i++)
+		current = current->next;
+	if (!current)
+		return (-1);
+
+	if (!*head || !current)
+		return (-1);
+	if (*head == current)
+		*head = current->next;
+	if (current->next)
+		current->prev->next = current->next;
+	free (current);
+	return (1);
 }
