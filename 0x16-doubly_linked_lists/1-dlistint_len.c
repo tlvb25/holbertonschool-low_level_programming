7e3f601aa87ceb15b3e6c9fb2f0ce271e8fb72f3
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-05 22:44:25 -0700

    code correction

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -8,14 +8,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *last;
 	unsigned int count = 0;
 
 
 	while (h)
 	{
 		count++;
-		last = h;
 		h = h->next;
 	}
 	return (count);
