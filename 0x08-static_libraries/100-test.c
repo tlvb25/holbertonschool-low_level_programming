168e1c79595a3011c786d44e69ab3e69109fb896
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-06 23:44:20 -0700

    file for 100 operations

---
+++ 0x08-static_libraries/100-test.c
@@ -0,0 +1,29 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+int quo(int a, int b)
+{
+	return (a % b);
+}
