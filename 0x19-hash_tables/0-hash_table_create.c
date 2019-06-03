1b94cd53016f82c546223210898ce7f66f053ed2
T <test@mail.com>
2019-06-02 19:43:49 -0700

    code style changes

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -3,21 +3,21 @@
 /**
  * hash_table_create -
  *
- * Return: 
+ * Return:
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *ht;
+	hash_table_t *ht;
 
-        ht = malloc(sizeof(hash_table_t));
-        if (!ht)
-                return (NULL);
-        ht->size = size;
-        ht->array = calloc(ht->size, sizeof(hash_node_t));
-        if (!ht->array)
-        {
-                free(ht);
-                return (NULL);
-        }
-        return (ht);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(ht->size, sizeof(hash_node_t));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
 }
