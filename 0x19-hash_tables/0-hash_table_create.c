218a290b84006ee77cec0c9bf9e7059bfa994632
T <test@mail.com>
2019-06-02 19:41:58 -0700

    code for task 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,5 +7,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+        hash_table_t *ht;
 
+        ht = malloc(sizeof(hash_table_t));
+        if (!ht)
+                return (NULL);
+        ht->size = size;
+        ht->array = calloc(ht->size, sizeof(hash_node_t));
+        if (!ht->array)
+        {
+                free(ht);
+                return (NULL);
+        }
+        return (ht);
 }
