e3e75b277082c9f2a972ecd3412787a96bb26f25
T <test@mail.com>
2019-06-02 20:15:49 -0700

    betty style changes

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * hash_djb2 - creates a hash table
  * @str: the string to be hashed
@@ -6,12 +8,10 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-    	int c;
+	int c;
 
-    	hash = 5381;
-    	while ((c = *str++))
-    	{
+	hash = 5381;
+	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    	}
-    	return (hash);
+	return (hash);
 }
