0f84316407f1989f90c8108391e875ac459a72e8
T <test@mail.com>
2019-06-02 22:31:14 -0700

    code for task 2

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
+ * key_index - returns index correlating to key
+ * @key: the string key given
+ * @size: max size of array
  *
- * Return: 
+ * Return: returns the index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+	unsigned long int hash = hash_djb2(key);
+	return (hash % size);
 }
--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,3 +9,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
 }
+
+unsigned long int index = hash_djb2(key);
+    
+    for(index = 0; index < size; index++) 
+    { 
+        if(key[index] == index) 
+            return index; 
+    } 
+    return (-1); 
