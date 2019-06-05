6c64437273b4c12cfaecf1b763072494f89beadf
T <test@mail.com>
2019-06-04 23:47:50 -0700

    betty corrections

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
+ * hash_table_create - creates a hast table (**array)
  * @size: the size of the array
+ *
  * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -12,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
+
 	ht->size = size;
 	ht->array = calloc(ht->size, sizeof(hash_node_t *));
 	if (!ht->array)
