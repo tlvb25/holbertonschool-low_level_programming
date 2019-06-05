477f351d09b8709605064196a43f4764442f8635
T <test@mail.com>
2019-06-04 22:45:43 -0700

    code for task 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,11 +13,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new = NULL;
 	hash_node_t *current = NULL;
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
-
+	unsigned long int index;
 	if (!ht || !key)
 		return (0);
-
+	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current)
 	{
