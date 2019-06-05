1e019316a3a2e63962c91441eaa6a22ee156b949
T <test@mail.com>
2019-06-04 22:26:18 -0700

    code for task 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -3,18 +3,47 @@
 /**
  * hash_table_set -
  *
- * Return: 
+ * Return:
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+        unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *new, *current;
 
+        if (key != NULL || ht != NULL)
+        {
+                return 0;
+	}
+        current = ht->array[index];
+        {
+		if (!strcmp(current->key, key))
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (!current->value)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+        new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
 }
-
-unsigned long int index = hash_djb2(key);
-    
-    for(index = 0; index < size; index++) 
-    { 
-        if(key[index] == index) 
-            return index; 
-    } 
-    return (-1); 
