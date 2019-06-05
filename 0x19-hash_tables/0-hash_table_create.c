4bed0cd3bf29c94eb4976115cb0c4642474e8b81
T <test@mail.com>
2019-06-04 23:50:40 -0700

    betty corrections

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hast table (**array)
- * @size: the size of the array
+ * hash_table_create - creates a hast table 
+ * @size: size of the array
  *
- * Return: pointer to the newly created hash table
+ * Return: newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
