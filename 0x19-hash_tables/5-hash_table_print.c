e8caebe6f609ca66d70a561724caa09afb3734a4
T <test@mail.com>
2019-06-04 23:32:49 -0700

    code for task 5

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -3,9 +3,32 @@
 /**
  * hash_table_print -
  *
- * Return: 
+ * Return:
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	int checker = 1;
+	hash_node_t *current;
 
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if (checker == 1)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+				checker = 0;
+			}
+			else
+				printf(", '%s': '%s'", current->key, current->value);
+			current = current->next;
+		}
+		i++;
+	}
+	printf("}\n");
 }
