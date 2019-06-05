847ce4cdc5d8d0c35e3dca893a3f21598f2212c2
T <test@mail.com>
2019-06-04 23:34:37 -0700

    betty style corrections

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table being printed
  *
- * Return:
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
