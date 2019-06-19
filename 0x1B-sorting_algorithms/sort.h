c1a7a510ce3042309302742f76cf1bcacad4db44
tlvb25 <638@holbertonschool.com>
2019-06-19 21:41:56 +0000

    adding file for project 0x1B. C - Sorting algorithms & Big O

---
+++ 0x1B-sorting_algorithms/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(int *array, size_t size)
+void insertion_sort_list(listint_t **list)
+void selection_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
+
+#endif /* SORT_H */
