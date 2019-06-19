5bedfbd236a1a032d875b45ab02ccaefb0933d4d
T <test@mail.com>
2019-06-19 15:50:07 -0700

    code for task 3

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,11 +1,44 @@
 #include "sort.h"
 
 /**
- * selection_sort -
- *
- * Return: 
+ * swap - swaps two integer numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - sorts a integer of array using selection sort algorithm
+ * @array: a integer of array
+ * @size: size of array
+ * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx;
 
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min_idx] > array[j])
+				min_idx = j;
+		}
+		if (i != min_idx)
+		{
+			swap(&array[i], &array[min_idx]);
+			print_array(array, size);
+		}
+	}
 }
--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,11 +1,87 @@
 #include "sort.h"
 
 /**
- * quick_sort -
- *
- * Return: 
+ * swap - swaps two integer numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - partition for quick sort algorithm
+ * @array: array need to sort
+ * @size: size
+ * @low: low bound index
+ * @high: high bound index
+ * Return: return pivot index
+ */
+size_t partition(int *array, size_t size, size_t low, size_t high)
+{
+	int pivot;
+	size_t j, i;
+
+	if (low >= high)
+		return (low);
+	pivot = array[high];
+	i = low;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (j != i)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != j)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quick_sort_3args - function takes 3 args
+ * @array: array to be sorted
+ * @size: size
+ * @low: low bound index
+ * @high: high bound index
+ * Return: void
+ */
+void quick_sort_3args(int *array, size_t size, size_t low, size_t high)
+{
+	size_t p;
+
+	if (low >= high)
+		return;
+	p = partition(array, size, low, high);
+	if (p != 0)
+		quick_sort_3args(array, size, low, p - 1);
+	if (p != high)
+		quick_sort_3args(array, size, p + 1, high);
+}
+
+/**
+ * quick_sort - sorts a integer of array using quick sort algorithm
+ * @array: a integer of array
+ * @size: size of array
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-
+	if (!array || size < 2)
+		return;
+	quick_sort_3args(array, size, 0, size - 1);
 }
