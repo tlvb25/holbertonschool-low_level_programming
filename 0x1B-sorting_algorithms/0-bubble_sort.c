c5d0c916886f957e575231b32dec6d07f518d853
T <test@mail.com>
2019-06-19 15:35:40 -0700

    code for task 1

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,11 +1,43 @@
 #include "sort.h"
 
 /**
- * bubble_sort -
- *
- * Return: 
+ * swap - swaps two integer numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sorts a integer of array using bubble sort algorithm
+ * @array: a integer of array
+ * @size: size of array
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j, flag = 0;
 
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1 && flag == 0; i++)
+	{
+		flag = 1;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				flag = 0;
+			}
+		}
+	}
 }
--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,11 +1,54 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list -
- *
- * Return: 
+ * swap_node - swaps two nodes instead swaps two numbers
+ * @list: head of linked list
+ * @node_a: node a
+ * @node_b: node b
+ * Return: void
+ */
+void swap_node(listint_t **list, listint_t **node_a, listint_t **node_b)
+{
+	listint_t *temp_prev, *temp_next;
+
+
+	temp_prev = (*node_a)->prev;
+	temp_next = (*node_b)->next;
+	if ((*node_a)->prev)
+		temp_prev->next = *node_b;
+	else
+		*list = *node_b;
+	if ((*node_b)->next)
+		temp_next->prev = *node_a;
+	(*node_a)->prev = *node_b;
+	(*node_b)->next = *node_a;
+	(*node_b)->prev = temp_prev;
+	(*node_a)->next = temp_next;
+}
+
+/**
+ * insertion_sort_list - sorts a integer of array using insertion algorithm
+ * @list: a integer of doublely linked list
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *node_a, *node_b, *current;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list)->next;
+	while (current)
+	{
+		node_b = current;
+		node_a = current->prev;
+		while (node_a && node_b->n < node_a->n)
+		{
+			swap_node(list, &node_a, &node_b);
+
+			node_a = node_b->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
