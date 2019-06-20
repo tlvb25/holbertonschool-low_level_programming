6c8708b154a1ed707d2f6dab3dcb33fd431e4744
T <test@mail.com>
2019-06-19 18:28:37 -0700

    code for task

---
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+
+/**
+ * heapify - a function recursively sorting to get root node largest
+ * @array: an array
+ * @index: index of node
+ * @heap_size: size of heap
+ * @size: size of the heap size
+ */
+
+/* Bill Huang Was My Partner for this Project, code is Similar */
+void heapify(int *array, size_t index, size_t heap_size, size_t size)
+{
+	int temp;
+	size_t largest, left_index, right_index;
+
+	largest = index;
+	left_index = 2 * index + 1;
+	right_index = 2 * index + 2;
+	if (left_index < heap_size && array[left_index] > array[largest])
+		largest = left_index;
+	if (right_index < heap_size && array[right_index] > array[largest])
+		largest = right_index;
+	if (largest != index)
+	{
+		temp = array[largest];
+		array[largest] = array[index];
+		array[index] = temp;
+		print_array(array, size);
+		heapify(array, largest, heap_size, size);
+	}
+}
+
+/**
+ * heap_sort - sorting use heap sort algorithm
+ * @array: an array need to be sort
+ * @size: size of the array
+ * Return: void
+ */
+
+/* Bill Huang Was My Partner for this Project, code is Similar */
+void heap_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i;
+
+	for (i = size / 2 - 1; ; i--)
+	{
+		heapify(array, i, size, size);
+		if (i == 0)
+			break;
+	}
+	for (i = size - 1; i > 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		heapify(array, 0, i, size);
+	}
+}
